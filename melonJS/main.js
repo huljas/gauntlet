5ca31a49fce1772230a6077411e6562a59b2bcdf
Heikki Uljas <huljas@gmail.com>
2011-09-18 22:22:37 +0300

    Added the melonJS tutorial

---
+++ melonJS/main.js
@@ -0,0 +1,96 @@
+/*!
+ * 
+ *   melonJS
+ *   http://www.melonjs.org
+ *		
+ *   Step by step game creation tutorial
+ *
+ **/
+
+// game resources
+var g_resources= [{
+	name: "area01_level_tiles",
+	type: "image",
+	src: "data/area01_tileset/area01_level_tiles.png"
+},{
+	name: "area01",
+	type: "tmx",
+	src: "data/area01.tmx"
+}];
+
+
+var jsApp	= 
+{	
+	/* ---
+	
+		Initialize the jsApp
+		
+		---			*/
+	onload: function()
+	{
+		
+      // init the video
+		if (!me.video.init('jsapp', 320, 480, false, 1.0))
+		{
+			alert("Sorry but your browser does not support html 5 canvas.");
+         return;
+		}
+				
+		// initialize the "audio"
+		me.audio.init("mp3,ogg");
+		
+		// set all resources to be loaded
+		me.loader.onload = this.loaded.bind(this);
+		
+		// set all resources to be loaded
+		me.loader.preload(g_resources);
+
+		// load everything & display a loading screen
+		me.state.change(me.state.LOADING);
+	},
+	
+	
+	/* ---
+	
+		callback when everything is loaded
+		
+		---										*/
+	loaded: function ()
+	{
+		// set the "Play/Ingame" Screen Object
+		me.state.set(me.state.PLAY, new PlayScreen());
+      
+      // start the game 
+		me.state.change(me.state.PLAY);
+	}
+
+}; // jsApp
+
+/* the in game stuff*/
+var PlayScreen = me.ScreenObject.extend(
+{
+
+   onResetEvent: function()
+   {	
+        me.levelDirector.loadLevel("area01");
+   },
+	
+	
+	/* ---
+	
+		 action to perform when game is finished (state change)
+		
+		---	*/
+	onDestroyEvent: function()
+	{
+	
+   }
+
+});
+
+
+//bootstrap :)
+window.onReady(function() 
+{
+	jsApp.onload();
+});
